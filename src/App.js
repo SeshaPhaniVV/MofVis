8d374b8f9ae26134e8338a08c6c14904e1996796
VVS Phani <phani.vakicherla@gmail.com>
2023-11-15 21:50:38 -0600

    Upgraded Table to use mui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import './App.css';
 import Molecules from './pages/Molecules';
 import Home from './pages/Home';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const router = createBrowserRouter([
   {
